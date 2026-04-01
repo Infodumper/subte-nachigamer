cb5299e7a84c021e85e8b48406995c410df7de7f
Ignacio <infodumper.au@gmail.com>
2026-04-01 15:58:53 -0300

    chore: ignore index.php and add .gitignore

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Este archivo sirve para que servidores locales como Laragon o XAMPP 
-// detecten correctamente la carpeta como un proyecto válido y lo muestren en su listado.
-require 'index.html';
-?>
